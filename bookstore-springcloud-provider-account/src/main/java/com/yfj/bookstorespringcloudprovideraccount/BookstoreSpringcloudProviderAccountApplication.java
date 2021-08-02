0e3a661edd863d9d7e2d0eda3f5cca1d8098f8f1
wwe1428103707 <2410712588@qq.com>
2021-08-02 16:14:19 +0800

    Initial commit

---
+++ bookstore-springcloud-provider-account/src/main/java/com/yfj/bookstorespringcloudprovideraccount/BookstoreSpringcloudProviderAccountApplication.java
@@ -0,0 +1,13 @@
+package com.yfj.bookstorespringcloudprovideraccount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreSpringcloudProviderAccountApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookstoreSpringcloudProviderAccountApplication.class, args);
+    }
+
+}
---
+++ bookstore-springcloud-provider-account/src/test/java/com/yfj/bookstorespringcloudprovideraccount/BookstoreSpringcloudProviderAccountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yfj.bookstorespringcloudprovideraccount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreSpringcloudProviderAccountApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
